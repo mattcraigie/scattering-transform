4ac98b7468cbcf022addc6ffa89002174f4d0530
mattcraigie <matthewcraigie1@gmail.com>
2022-09-05 16:06:59 +1000

    Adding angular reduction

--- scattering_transform/scattering_transform.py
+++ scattering_transform/scattering_transform.py
@@ -110,11 +110,11 @@ class ScatteringTransformFast(torch.nn.Module):
 
             self.s2 = torch.zeros((batch_size, self.J, self.J, self.num_l_deltas), device=self.filters.device)
             self.S2 = self.S2.permute((0, 1, 3, 2, 4))
-            print(self.S2[0, 0, 0])
+            print(self.S2[0, 1, 4])
             for i in range(self.num_l_deltas):
                 print(self.S2[:, :, :, self.l_deltas_masks[i]].shape)
                 self.s2[..., i] = torch.mean(self.S2[:, :, :, self.l_deltas_masks[i]], dim=-1)
-            print(self.s2[0, 0, 0])
+            print(self.s2[0, 1, 4])
 
         else:
 
