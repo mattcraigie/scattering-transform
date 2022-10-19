05eadcaa2163a5ac9f4d32a95a94ca8f881d58ef
mattcraigie <matthewcraigie1@gmail.com>
2022-10-19 16:28:13 +1000

    debuggigng

--- scattering_transform/wavelet_models.py
+++ scattering_transform/wavelet_models.py
@@ -41,6 +41,8 @@ class Wavelet(object):
                 self.filter_tensor[scale, angle] = self.wavelet_function(scale, angle)
 
     def to(self, device):
+        print(self.filter_tensor)
+        print(type(self.filter_tensor))
         self.filter_tensor = self.filter_tensor.to(device)
 
 
@@ -90,3 +92,6 @@ class Learnable(Morlet, torch.nn.Module):
 
     def forward(self):
         return
+
+    # def to(self, device):
+    #     self.filter_tensor = self.filter_tensor.to(device)
