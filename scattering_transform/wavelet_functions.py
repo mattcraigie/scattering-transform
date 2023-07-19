5008b398194dcdaa3362a54eeb555b810dfe0c77
mattcraigie <matthewcraigie1@gmail.com>
2023-07-19 11:32:20 +1000

    Added skew wavelets

--- scattering_transform/wavelet_functions.py
+++ scattering_transform/wavelet_functions.py
@@ -100,7 +100,7 @@ def skew_wavelet(size, scale, angle, num_scales, num_angles):
     x = np.array([xx, yy]).swapaxes(0, 2).swapaxes(0, 1)[..., np.newaxis]
 
     theta = torch.tensor(2 * (int(num_angles - num_angles / 2 - 1) - angle) * torch.pi / num_angles)
-    x_rotated = x @ rotation_matrix(theta)
+    x_rotated = x @ rotation_matrix(theta).numpy()
     x_scaled_and_rotated = x_rotated * 2 ** scale
 
     wavelet = skew_normal(x_scaled_and_rotated, alpha) * complex_sinusoid(x_scaled_and_rotated, beta)
