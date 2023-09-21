1721722f316b8397cfefec6276c0588ecd42a33a
mcraigie <matthewcraigie1@gmail.com>
2023-09-21 16:23:40 +1000

    Improving commenting

---
+++ scattering_transform/reducer.py
@@ -0,0 +1,102 @@
+import torch
+from .scattering_transform import ScatteringTransform2d
+
+
+class Reducer(torch.nn.Module):
+
+    # todo: add in a method that takes in an index and outputs the J1, L1, J2, L2 etc that it corresponds to.
+
+    def __init__(self, filters, reduction, normalise_s2=False):
+        """Class that reduces the scattering coefficients based on the symmetry in the field. It also performs some
+        normalisation. The main reason to use a class rather than a function is so that we can pre-calculate the
+        number of scattering coefficients, given the reduction.
+
+        Note that the first order coefficients are independent of the mean of the field, since by the admissibility
+        criterion, the zero-freq component of the filter is always zero (i.e. any info about the mean is destroyed).
+        Therefore, normalising by dividing out S0 always has the effect of correlating, instead of de-correlating.
+        Which is why I've removed it as an option.
+
+        An alternative to the normalisation that I could implement in the future, is to set a normalisation factor such
+        that instances of a GRF of that size would have standard normally distributed coefficients. The goal being that
+        these outputs will then be distributed approximately between (-3, 3) and therefore be well-behaved as inputs
+        to neural networks, without requiring batch normalisation. An added benefit is that it will be immediately clear
+        whether the field has more or less `scattering power' through that filter.
+        """
+        super(Reducer, self).__init__()
+
+        assert reduction in [None, 'none', 'rot_avg', 'ang_avg', 'asymm_ang_avg'], \
+            "Wrong reduction: must be one of [None (or 'none'), 'rot_avg', 'ang_avg', 'asymm_ang_avg']"
+
+        assert filters.num_angles % 2 == 0, "Number of angles must be even for now."
+
+        self.reduction = reduction
+        self.normalise_s2 = normalise_s2
+
+        # run a test field through the scattering transform to work out the required number of outputs
+        test_field = torch.randn(1, 1, filters.size, filters.size).to(filters.device)
+        st = ScatteringTransform2d(filters)
+        st.to(filters.device)
+        s = st.forward(test_field)
+        self.num_outputs = self.forward(s).shape[-1]
+
+    def forward(self, s):
+
+        s0, s1, s2 = s
+
+        # shorthands for cleaner code
+        sln = slice(None)  # SLice None, i.e. :
+        batch_dims = s0.ndim - 1
+        batch_sizes = list(s0.shape[:-1])
+        bds = [sln]*batch_dims  # Batch Dim Slice nones
+
+        # Normalisation
+        if self.normalise_s2:
+            s2 = s2 / s1[bds + [sln, sln, None, None]]
+
+        # Selecting only j2 > j1 entries
+        scale_idx = torch.triu_indices(s1.shape[-2], s1.shape[-2], offset=1)
+        s2 = s2[bds + [scale_idx[0], sln, scale_idx[1]]]
+        s2 = s2.permute([i + 1 for i in range(batch_dims)] + [0, s2.ndim-2, s2.ndim-1])
+
+        if self.reduction is None or self.reduction == 'none':
+            s1 = s1.flatten(-2, -1)
+            s2 = s2.flatten(-3, -1)
+
+        elif self.reduction == 'rot_avg':
+            s1 = s1.mean(-1)
+            s2 = s2.mean(dim=(-2, -1))
+
+        elif self.reduction == 'ang_avg':
+            s1 = s1.mean(-1)
+            num_angles = s2.shape[-1]
+
+            # Calculate the distance between each angle in a 2D tensor. Angles left are equivalent to angles right,
+            # which is why we take the absolute value.
+            delta = torch.abs(torch.arange(num_angles)[:, None] - torch.arange(num_angles)[None, :])
+
+            # Use the rotational symmetry of the filters (e.g. for J=4, 3 angles apart is equivalent to 1 angle apart,
+            # when you got the other way, so we can just take the min of the two).
+            delta = torch.min(num_angles - delta, delta)
+
+            s2_vals_all = []
+            for i in range(delta.min(), delta.max()+1):
+                idx = torch.where(delta == i)  # find the indices of the angles that are i apart
+                s2_vals_all.append(s2[bds + [sln, idx[0], idx[1]]].mean(-1))  # take the average of everything with the same angle difference
+            s2 = torch.cat(s2_vals_all, dim=-1)
+
+        elif self.reduction == 'asymm_ang_avg':
+            s1 = s1.mean(-1)
+            num_angles = s2.shape[-1]
+
+            # calculate the distance between each angle in a 2D tensor. Due to asymmetry, angles left are not equivalent
+            # to angles right.
+            delta = torch.arange(num_angles)[:, None] - torch.arange(num_angles)[None, :]
+
+            s2_vals_all = []
+            for i in range(delta.min(), delta.max()+1):
+                idx = torch.where(delta == i)  # find the indices of the angles that are i apart
+                part = s2[bds + [sln, idx[0], idx[1]]]
+                s2_vals_all.append(part.mean(-1))
+            s2 = torch.cat(s2_vals_all, dim=-1)
+
+        return torch.cat((s0, s1, s2), dim=batch_dims)
--- scattering_transform/scattering_transform.py
+++ scattering_transform/scattering_transform.py
@@ -14,110 +14,15 @@ def scattering_operation(input_a: torch.Tensor, input_b: torch.Tensor) -> torch.
     return ifft2(input_a * input_b).abs()
 
 
-
-
-class Reducer(torch.nn.Module):
-
-    # todo: add in a method that takes in an index and outputs the J1, L1, J2, L2 etc that it corresponds to.
-
-    def __init__(self, filters, reduction, normalise_s2=False):
-        """Class that reduces the scattering coefficients based on the symmetry in the field. It also performs some
-        normalisation. The main reason to use a class rather than a function is so that we can pre-calculate the
-        number of scattering coefficients, given the reduction.
-
-        Note that the first order coefficients are independent of the mean of the field, since by the admissibility
-        criterion, the zero-freq component of the filter is always zero (i.e. any info about the mean is destroyed).
-        Therefore, normalising by dividing out S0 always has the effect of correlating, instead of de-correlating.
-        Which is why I've removed it as an option.
-
-        An alternative to the normalisation that I could implement in the future, is to set a normalisation factor such
-        that instances of a GRF of that size would have standard normally distributed coefficients. The goal being that
-        these outputs will then be distributed approximately between (-3, 3) and therefore be well-behaved as inputs
-        to neural networks, without requiring batch normalisation. An added benefit is that it will be immediately clear
-        whether the field has more or less `scattering power' through that filter.
-        """
-        super(Reducer, self).__init__()
-
-        assert reduction in [None, 'none', 'rot_avg', 'ang_avg', 'asymm_ang_avg'], \
-            "Wrong reduction: must be one of [None (or 'none'), 'rot_avg', 'ang_avg', 'asymm_ang_avg']"
-
-        assert filters.num_angles % 2 == 0, "Number of angles must be even for now."
-
-        self.reduction = reduction
-        self.normalise_s2 = normalise_s2
-
-        # run a test field through the scattering transform to work out the required number of outputs
-        test_field = torch.randn(1, 1, filters.size, filters.size).to(filters.device)
-        st = ScatteringTransform2d(filters)
-        st.to(filters.device)
-        s = st.forward(test_field)
-        self.num_outputs = self.forward(s).shape[-1]
-
-    def forward(self, s):
-
-        s0, s1, s2 = s
-
-        # shorthands for cleaner code
-        sln = slice(None)  # SLice None, i.e. :
-        batch_dims = s0.ndim - 1
-        batch_sizes = list(s0.shape[:-1])
-        bds = [sln]*batch_dims  # Batch Dim Slice nones
-
-        # Normalisation
-        if self.normalise_s2:
-            s2 = s2 / s1[bds + [sln, sln, None, None]]
-
-        # Selecting only j2 > j1 entries
-        scale_idx = torch.triu_indices(s1.shape[-2], s1.shape[-2], offset=1)
-        s2 = s2[bds + [scale_idx[0], sln, scale_idx[1]]]
-        s2 = s2.permute([i + 1 for i in range(batch_dims)] + [0, s2.ndim-2, s2.ndim-1])
-
-        if self.reduction is None or self.reduction == 'none':
-            s1 = s1.flatten(-2, -1)
-            s2 = s2.flatten(-3, -1)
-
-        elif self.reduction == 'rot_avg':
-            s1 = s1.mean(-1)
-            s2 = s2.mean(dim=(-2, -1))
-
-        elif self.reduction == 'ang_avg':
-            s1 = s1.mean(-1)
-            num_angles = s2.shape[-1]
-
-            # Calculate the distance between each angle in a 2D tensor. Angles left are equivalent to angles right,
-            # which is why we take the absolute value.
-            delta = torch.abs(torch.arange(num_angles)[:, None] - torch.arange(num_angles)[None, :])
-
-            # Use the rotational symmetry of the filters (e.g. for J=4, 3 angles apart is equivalent to 1 angle apart,
-            # when you got the other way, so we can just take the min of the two).
-            delta = torch.min(num_angles - delta, delta)
-
-            s2_vals_all = []
-            for i in range(delta.min(), delta.max()+1):
-                idx = torch.where(delta == i)  # find the indices of the angles that are i apart
-                s2_vals_all.append(s2[bds + [sln, idx[0], idx[1]]].mean(-1))  # take the average of everything with the same angle difference
-            s2 = torch.cat(s2_vals_all, dim=-1)
-
-        elif self.reduction == 'asymm_ang_avg':
-            s1 = s1.mean(-1)
-            num_angles = s2.shape[-1]
-
-            # calculate the distance between each angle in a 2D tensor. Due to asymmetry, angles left are not equivalent
-            # to angles right.
-            delta = torch.arange(num_angles)[:, None] - torch.arange(num_angles)[None, :]
-
-            s2_vals_all = []
-            for i in range(delta.min(), delta.max()+1):
-                idx = torch.where(delta == i)  # find the indices of the angles that are i apart
-                part = s2[bds + [sln, idx[0], idx[1]]]
-                s2_vals_all.append(part.mean(-1))
-            s2 = torch.cat(s2_vals_all, dim=-1)
-
-        return torch.cat((s0, s1, s2), dim=batch_dims)
-
-
 class ScatteringTransform2d(torch.nn.Module):
     def __init__(self, filter_bank: FilterBank):
+        """
+        A class to compute the scattering transform of a 2D field, for a given set of filters stored in a FilterBank
+        class.
+
+        :param filter_bank: The filter bank to use for the scattering transform.
+        """
+
         super(ScatteringTransform2d, self).__init__()
 
         # check the filter bank has been correctly specified
@@ -130,8 +35,13 @@ class ScatteringTransform2d(torch.nn.Module):
                                      for j in range(self.filter_bank.num_scales)]
         self.clip_scaling_factors = torch.tensor(self.clip_scaling_factors)
 
-    def scattering_transform(self, fields):
-        # fields should be a tensor of shape (batch, channels, size, size)
+    def _scattering_transform(self, fields):
+        """
+        Computes the scattering transform of a batch of 2D fields. Use the forward method of the class to call.
+        :param fields: A tensor of shape (batch, channels, size, size) containing the fields to transform.
+        :return: A tuple containing the zeroth, first and second order scattering coefficients.
+        """
+
         assert len(fields.shape) == 4, "The input fields should have shape (batch, channels, size, size)"
 
         # the zeroth order coefficient
@@ -145,6 +55,11 @@ class ScatteringTransform2d(torch.nn.Module):
         return coeffs_0, coeffs_1, coeffs_2
 
     def _first_order(self, fields):
+        """
+        Computes the first order scattering coefficients and the first order scattering fields.
+        :param fields: A tensor of shape (batch, channels, size, size) containing the fields to transform.
+        :return: A tuple containing the first order scattering coefficients and the first order scattering fields.
+        """
 
         fields_fourier = fft2(fields)
 
@@ -176,7 +91,11 @@ class ScatteringTransform2d(torch.nn.Module):
         return first_order_coefficients, first_order_scattering_fields
 
     def _second_order(self, field_list):
-
+        """
+        Computes the second order scattering coefficients.
+        :param field_list: A list of tensors containing the first order scattering fields.
+        :return: A tensor containing the second order scattering coefficients.
+        """
         second_order_coefficients = []
         for scale_1 in range(self.filter_bank.num_scales):
             for scale_2 in range(self.filter_bank.num_scales):
@@ -217,6 +136,12 @@ class ScatteringTransform2d(torch.nn.Module):
         return second_order_coefficients
 
     def _rescale_coeffs(self, c1, c2):
+        """
+        Rescales the scattering coefficients to account for the effects of clipping.
+        :param c1: the first order scattering coefficients
+        :param c2: the second order scattering coefficients
+        :return: a tuple containing the rescaled coefficients
+        """
 
         c1 = c1 * self.clip_scaling_factors[None, None, :, None]
         c2 = c2 * self.clip_scaling_factors[None, None, None, None, :, None]
@@ -224,10 +149,21 @@ class ScatteringTransform2d(torch.nn.Module):
         return c1, c2
 
     def forward(self, fields):
-        return self.scattering_transform(fields)
+        """
+        The forward pass of the scattering transform.
+        :param fields: A tensor of shape (batch, channels, size, size) containing the fields to transform.
+        :return: A tuple containing the zeroth, first and second order scattering coefficients.
+        """
+        return self._scattering_transform(fields)
 
     def to(self, device):
+        """
+        Moves the scattering transform to the specified device.
+        :param device: the device to move to
+        :return: self
+        """
         super(ScatteringTransform2d, self).to(device)
         self.filter_bank.to(device)
         self.clip_scaling_factors = self.clip_scaling_factors.to(device)
         return self
+
